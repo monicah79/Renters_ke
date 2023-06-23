38494b7e62fdf2b048845f3ebc720f06796d39da
monicah79 <monniewanjema@gmail.com>
2023-06-24 00:39:59 +0300

    Requested changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # controllers: { registrations: "users/registrations" }
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   root "user#index"
-  # get '/error', to: 'errors#error_404', as: :error
-  devise_for :users, skip: :all
   resources :user, only: [:index, :show] do
     resources :post, only: [:index, :show] 
     resources :comment, only: [:new, :create]
-    # get 'post', on: :member
-  
+  end
+  devise_scope :user do
+    post 'users/sign_up', to: 'users/registrations#create'
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,10 +3,8 @@ require 'rails_helper'
 RSpec.describe Comment, type: :model do
   describe '#update_comments_counter' do
     it 'increments comments_counter by 1' do
-      new_user = User.create(name: 'Melissa', photo: 'https://picsum.photos/300/200', bio: 'Teacher from Mexico.',
-                             posts_counter: 3),
-
-                 new_user.save
+      User.create(name: 'Melissa', photo: 'https://picsum.photos/300/200', bio: 'Teacher from Mexico.',
+                  posts_counter: 3)
       post = new_user.posts.build(title: 'Test post', text: 'This is a test post', comments_counter: 0,
                                   likes_counter: 0)
       post.save
