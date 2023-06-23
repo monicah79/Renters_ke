1c9006ef64c0a3da1b79b54812566ccf2a993a83
monicah79 <monniewanjema@gmail.com>
2023-06-23 18:48:58 +0300

    Fixed rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def after_sign_up_path_for(resource)
+  def after_sign_up_path_for(_resource)
     new_user_session_path
   end
 
