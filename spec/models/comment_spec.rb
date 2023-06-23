df5a1b0dafe00be2f94ebbad600c608393013b50
monicah79 <monniewanjema@gmail.com>
2023-06-23 17:08:39 +0300

    complete devise project

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe Comment, type: :model do
   describe '#update_comments_counter' do
     it 'increments comments_counter by 1' do
       new_user = User.create(name: 'Melissa', photo: 'https://picsum.photos/300/200', bio: 'Teacher from Mexico.',
-      posts_counter: 3),
+                             posts_counter: 3),
 
-      new_user.save
+                 new_user.save
       post = new_user.posts.build(title: 'Test post', text: 'This is a test post', comments_counter: 0,
                                   likes_counter: 0)
       post.save
